bfd63b4a44ccce4de3e079af04c598cbc381e922
joyadeep <joyadeeplimbu@gmail.com>
2023-08-17 08:08:00 +0545

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import {motion} from 'framer-motion'
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-3xl font-semibold'>Start your project</h1>
-    </main>
+    <>
+      <Navbar/>
+    </>
   )
 }
